ecd8ac4fb1d6411a4861af36dff2e533076c4e5e
Phaneendrasai <alluruphaneendrasai@gmail.com>
2023-04-02 08:04:00 +0530

    Added Description, Images and links

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
+df = pd.read_csv('data.csv', sep=';')
+print(df)
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -19,3 +22,21 @@ with col2:
     """
     st.info(content)
 
+content2 = """This is a section for my portfolio applications"""
+st.write(content2)
+
+col3,empty_col,  col4 = st.columns([1.5, 0.5, 1.5])
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write("[Source Code](https://www.google.com)")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](https://www.google.com)")
